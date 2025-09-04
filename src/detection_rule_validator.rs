5437fa5ea66b3b269d18c265be9c19b6fbe21ee8
Mark Sayson <masayson@gmail.com>
2025-09-03 21:21:28 -0700

    Add CLI argument validation against the request allow-list

--- src/detection_rule_validator.rs
+++ src/detection_rule_validator.rs
@@ -52,15 +52,65 @@ fn validate_request_allow_list_schema(
     )
 }
 
+fn parse_cli_command(cli_request: &serde_json::Value) -> Result<String, String> {
+    let command_value = cli_request
+        .get("command")
+        .ok_or_else(|| "CLI request command not found".to_string())?;
+    let cli_command: &str = command_value
+        .as_str()
+        .ok_or_else(|| "CLI request command {command_value:?} is not a string".to_string())?;
+    Ok(cli_command.to_string())
+}
+
+fn validate_cli_args_match_allow_list(
+    cli_command: &str,
+    cli_request: &serde_json::Value,
+    allow_listed_requests_for_cli_command: Vec<&serde_json::Value>,
+) -> Result<(), String> {
+    let optional_cli_args = cli_request.get("args");
+    if optional_cli_args.is_none() {
+        return Ok(());
+    }
+    let cli_args = optional_cli_args.unwrap().as_array().unwrap();
+
+    for allowed_cli_request in allow_listed_requests_for_cli_command {
+        let exact_args = allowed_cli_request.get("exactArgs");
+        let initial_args = allowed_cli_request.get("initialArgs");
+
+        match (exact_args, initial_args) {
+            (Some(exact_args), _) => {
+                let exact_args_array = exact_args.as_array().unwrap();
+                if exact_args_array == cli_args {
+                    return Ok(());
+                }
+            }
+            (None, Some(initial_args)) => {
+                let initial_args_array = initial_args.as_array().unwrap();
+                if cli_args.starts_with(initial_args_array) {
+                    return Ok(());
+                }
+            }
+            (None, None) => {
+                // If neither exactArgs nor initialArgs is specified, any args are allowed
+                return Ok(());
+            }
+        }
+    }
+
+    Err(format!(
+        "Validation error: CLI command '{cli_command}' with arguments '{cli_args:#?}' is not allowed by the request allow-list"
+    ))
+}
+
 fn validate_cli_request_matches_allow_list(
     cli_request: &serde_json::Value,
     allow_listed_cli_requests: Option<&Vec<serde_json::Value>>,
 ) -> Result<(), String> {
-    let cli_command = cli_request.get("command").unwrap().as_str().unwrap();
+    let cli_command = parse_cli_command(cli_request)?;
 
     if allow_listed_cli_requests.is_none() {
         return Err(format!(
-            "Validation error: CLI command '{cli_command}' is not allowed by the request allow-list"
+            "Validation error: CLI command '{cli_command}' is not allowed, no allow-listed CLI commands"
         ));
     }
     let allow_listed_cli_requests_matching_cli_command = allow_listed_cli_requests
@@ -73,15 +123,25 @@ fn validate_cli_request_matches_allow_list(
                 .as_str()
                 .unwrap()
                 == cli_command
-        });
-    if allow_listed_cli_requests_matching_cli_command.count() == 0 {
+        })
+        .collect::<Vec<&serde_json::Value>>();
+
+    if allow_listed_cli_requests_matching_cli_command.is_empty() {
         return Err(format!(
-            "Validation error: CLI command '{cli_command}' is not allowed by the request allow-list"
+            "Validation error: CLI command '{cli_command}' is not allowed, allow-listed CLI commands: {:?}",
+            allow_listed_cli_requests
+                .unwrap()
+                .iter()
+                .map(|request| request.get("command").unwrap().as_str().unwrap())
+                .collect::<Vec<&str>>()
         ));
     }
 
-    // TODO: validate CLI command arguments against allow-list
-    Ok(())
+    validate_cli_args_match_allow_list(
+        &cli_command,
+        cli_request,
+        allow_listed_cli_requests_matching_cli_command,
+    )
 }
 
 fn validate_detection_rule_matches_request_allow_list(
@@ -172,7 +232,11 @@ mod tests {
         .unwrap();
 
         let result = validate_detection_rule_data(&detection_rule_json, None);
-        assert!(result.is_ok());
+        assert!(
+            result.is_ok(),
+            "Unexpected validation error: {:?}",
+            result.unwrap_err()
+        );
     }
 
     #[test]
@@ -244,7 +308,11 @@ mod tests {
 
         let result =
             validate_detection_rule_data(&detection_rule_json, Some(&request_allow_list_json));
-        assert!(result.is_ok());
+        assert!(
+            result.is_ok(),
+            "Unexpected validation error: {:?}",
+            result.unwrap_err()
+        );
     }
 
     #[test]
@@ -265,7 +333,7 @@ mod tests {
         assert!(result.is_err());
         assert_eq!(
             result.unwrap_err(),
-            "Validation error: CLI command 'ls' is not allowed by the request allow-list"
+            "Validation error: CLI command 'ls' is not allowed, allow-listed CLI commands: [\"echo\"]"
         );
     }
 
@@ -284,6 +352,81 @@ mod tests {
 
         let result =
             validate_detection_rule_data(&detection_rule_json, Some(&request_allow_list_json));
-        assert!(result.is_ok());
+        assert!(
+            result.is_ok(),
+            "Unexpected validation error: {:?}",
+            result.unwrap_err()
+        );
+    }
+
+    #[test]
+    fn validate_detection_rule_data_rejects_non_exact_cli_command_args() {
+        let detection_rule_json: serde_json::Value = parse_json_from_filepath(
+            "resources/test/valid_detector_rules/extra_cli_args_rule.json",
+            "detection rule",
+        )
+        .unwrap();
+        let request_allow_list_json: serde_json::Value = parse_json_from_filepath(
+            "resources/test/valid_request_allow_lists/exact_cli_args_allow_list.json",
+            "request allow-list",
+        )
+        .unwrap();
+
+        let result =
+            validate_detection_rule_data(&detection_rule_json, Some(&request_allow_list_json));
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err(),
+            "Validation error: CLI command 'echo' with arguments '[\n    String(\"Hello\"),\n    String(\"world!\"),\n    String(\"My name is HAL.\"),\n]' is not allowed by the request allow-list"
+        );
+    }
+
+    #[test]
+    fn validate_detection_rule_data_passes_initial_allowed_cli_command_args() {
+        for input_file_path in [
+            "resources/test/valid_detector_rules/exact_allowed_cli_args_rule.json",
+            "resources/test/valid_detector_rules/extra_cli_args_rule.json",
+        ] {
+            let detection_rule_json: serde_json::Value =
+                parse_json_from_filepath(input_file_path, "detection rule").unwrap();
+            let request_allow_list_json: serde_json::Value = parse_json_from_filepath(
+                "resources/test/valid_request_allow_lists/initial_cli_args_allow_list.json",
+                "request allow-list",
+            )
+            .unwrap();
+
+            let result =
+                validate_detection_rule_data(&detection_rule_json, Some(&request_allow_list_json));
+            assert!(
+                result.is_ok(),
+                "Unexpected validation error for input file '{input_file_path}': {:?}",
+                result.unwrap_err()
+            );
+        }
+    }
+
+    #[test]
+    fn validate_detection_rule_data_rejects_missing_initial_allowed_cli_command_args() {
+        for input_file_path in [
+            "resources/test/valid_detector_rules/simple_no_op_rule.json",
+            "resources/test/valid_detector_rules/missing_initial_cli_args_rule.json",
+        ] {
+            let detection_rule_json: serde_json::Value =
+                parse_json_from_filepath(input_file_path, "detection rule").unwrap();
+            let request_allow_list_json: serde_json::Value = parse_json_from_filepath(
+                "resources/test/valid_request_allow_lists/initial_cli_args_allow_list.json",
+                "request allow-list",
+            )
+            .unwrap();
+
+            let result =
+                validate_detection_rule_data(&detection_rule_json, Some(&request_allow_list_json));
+            assert!(result.is_err());
+            assert!(
+                result
+                    .unwrap_err()
+                    .starts_with("Validation error: CLI command 'echo' with arguments")
+            );
+        }
     }
 }
